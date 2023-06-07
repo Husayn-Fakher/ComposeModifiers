0e79c788a5347a1d5331e20e1b53fb5d3a6f16e1
Husayn-Fakher <husayn.fakher@gmail.com>
2023-06-07 13:21:40 +0200

    Modifiers

---
+++ app/src/main/java/my/app/modifiers/MainActivity.kt
@@ -0,0 +1,93 @@
+package my.app.modifiers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import my.app.modifiers.ui.theme.ModifiersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ModifiersTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                    DemoScreen()
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+
+fun DemoScreen(){
+
+    val modifier = Modifier
+        .padding(all = 10.dp)
+        .border(width = 2.dp, color = Color.Black)
+
+    val secondModifier = Modifier.height(50.dp)
+
+    Column(
+        Modifier.padding(20.dp),
+
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Text("Calling Someone", modifier = modifier.then(secondModifier), fontSize = 35.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(Modifier.height(16.dp))
+
+        CustomImage(image =R.drawable.calling_someone, Modifier.padding(16.dp).width(270.dp).clip(shape = RoundedCornerShape(30.dp)))
+
+    }
+
+
+}
+
+@Composable
+
+fun CustomImage(image: Int, modifier: Modifier = Modifier){
+
+    Image(
+        painter = painterResource(image),
+        contentDescription = null,
+        modifier
+    )
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ModifiersTheme {
+       DemoScreen()
+    }
+}
